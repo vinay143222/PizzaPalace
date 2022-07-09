f11e7caaa6df972779b156b15997239fd95ed93f
vinay kumar <vinaykumarhmvini@gmail.com>
2022-07-09 12:34:10 +0530

    Reducer used in home screen

--- client/src/components/HomeScreen.js
+++ client/src/components/HomeScreen.js
@@ -1,25 +1,49 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 import Button from 'react-bootstrap/Button';
 
 import {Link} from 'react-router-dom';
 
 import axios from 'axios';
+const reducer=(state,action)=>{
+    switch(action.type)
+    {
+        case 'FETCH_REQUEST': return {...state,loading:true};
+        case 'FETCH_SUCCESS':return {...state,pizzas:action.payload,loading:false};
+        case 'FETCH_FAIL':return {...state,loading:false,error:action.payload};
+        default: return state;
+    }
+}
 function Home() {
-    const [pizzas,setpizza]=useState([]);
+  const [{loading,error,pizzas,},dispatch]=useReducer(reducer,{
+    pizzas:[],
+    loading:true,
+    error:[],
+
+  })
 
     useEffect(() => {
         const fetchData=async()=>{
-            const result=await axios.get('/api/products');
-            setpizza(result.data);
+            dispatch({type:'FETCH_REQUEST'});
+            try {
+                 const result=await axios.get('/api/products');
+                 dispatch({type:'FETCH_SUCCESS',payload:result.data})
+            } catch (error) {
+                dispatch({type:'FETCH_FAIL',payload:error.message})
+            }
+           
+           
         };
         fetchData();
     }, [])
-    console.log(pizzas)
+   
     return (
         <div>
             <h1 style={{ textAlign: 'center' }}>PIZZA MENU</h1>
             <div className="products">
-                {pizzas.map((Pizza) => {
+                
+                {
+                 loading ? (<div>Loading ...</div>):error?(<div>error</div>):(
+                pizzas.map((Pizza) => {
                     return (
                         <div className="product">
                             <Link to={`/product/${Pizza.id}`}>
@@ -35,8 +59,10 @@ function Home() {
                             </div>
                         </div>
                     )
-                })}
+                }))}
+                
             </div>
+            
         </div>
     )
 }
