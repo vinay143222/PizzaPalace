6f9f310f794c6e59736176a4a2500ea7a25336c9
vinay kumar <vinaykumarhmvini@gmail.com>
2022-07-16 10:56:45 +0530

    installed dotenv connected to db

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log("connected to db")
+}).catch(err => {
+    console.log(err.message)
+})
 const app = express();
 app.get("/api/products", (req, res) => {
     res.send(data);
