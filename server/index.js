60f860288baefbb8d672ba33de532aebd89aa271
vinay kumar <vinaykumarhmvini@gmail.com>
2022-07-16 20:05:48 +0530

    added sigin functionalites

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import cors from 'cors'
 import seedRouter from './routes/seedRoutes.js';
 import pizzaRouter from './routes/pizzaRoutes.js';
+import userRouter from './routes/userRoutes.js';
 dotenv.config();
 mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log("connected to db")
@@ -17,7 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/seed', seedRouter);
 app.use('/api/products', pizzaRouter);
-
+app.use('/api/users', userRouter);
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
+});
 
 app.listen(5000, () => {
     console.log("server is started");
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import User from '../models/users.js';
+import bcrypt from 'bcryptjs';
+import { generateToken } from '../utlis.js';
+import expressAsyncHandler from 'express-async-handler';
+const userRouter = express.Router();
+userRouter.post('/sigin', expressAsyncHandler(async(req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+            res.send({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                token: generateToken(user)
+            });
+            return;
+        }
+    }
+    res.status(401).send({ message: 'Invaild  email or password' })
+
+}));
+export default userRouter;
